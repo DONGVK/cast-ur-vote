e89dd4086279ca371369aa2b1f54fbb4326c335b
DONGVK <57867631+DONGVK@users.noreply.github.com>
2021-06-18 11:19:56 +0200

    'Update'

--- app.py
+++ app.py
@@ -4,5 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "cas'ur'vote")
+    name = request.args.get("name", "cas-ur-vote")
     return f'Bienvenue chez {escape(name)} !'
+
+@app.route('/singup', methods=['POST'])
+def signUp():
+    if request.method == 'POST':
+        type = request.args.get('type')
+        if type == "utilisateur":
+            return f'User route'
+        elif type == "week":
+            return f'Candidate route'
+        else :
+            return f'Please change the method or the parameters'
+    else:
+        return f'Please change the method or the parameters'
